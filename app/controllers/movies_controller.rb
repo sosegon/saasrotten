bc5ad2dd3855fd20974e42ae16df0fae22754d33
sosegon <anse23@hotmail.com>
2012-10-28 13:09:50 -0700

    part 1 and 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,9 +7,37 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+  	@all_ratings = Movie.get_all_ratings
+  	
+  	if !params.has_key? "ratings"
+  		@checked_ratings = @all_ratings
+  	else
+  		@checked_ratings = params[:ratings].keys
+  	end
+  	
+		sort_by = params[:sort_by]		
+		
+		if sort_by.eql? 'title'
+			@checked_ratings = flash[:ratings]
+			@movies = Movie.order('title ASC').get_movies_by_rating(@checked_ratings)
+			@title_css = 'hilite'
+			@date_css = ''
+			
+		elsif sort_by.eql? 'release_date'
+			@checked_ratings = flash[:ratings]
+			@movies = Movie.order('release_date ASC').get_movies_by_rating(@checked_ratings)
+			@date_css = 'hilite'
+			@title_css = ''
+			
+		else
+			@movies = Movie.get_movies_by_rating(@checked_ratings)
+			@date_css = ''
+			@title_css = ''
+		end
+			
+		flash[:ratings] = @checked_ratings
   end
-
+	
   def new
     # default: render 'new' template
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,23 @@
 class Movie < ActiveRecord::Base
+	def self.get_all_ratings
+		movies = self.all
+		ratings = []
+		movies.each do|m|
+			if !ratings.include? m.rating
+				ratings << m.rating
+			end
+		end
+		return ratings
+	end
+	
+	def self.get_movies_by_rating(ratings)
+		movies = []
+		ratings.each do |r|
+			self.find_all_by_rating(r).each do |m|
+				movies.push(m)
+			end
+		end
+		return movies
+	end
+
 end
